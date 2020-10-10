723954e86ead5c311c9141d6601921d42d301fd7
Chi-Ming Yip <cyip0002@student.monash.edu>
2020-10-10 12:01:38 +1100

    WIP: Installing Amplify

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from './navigation';
 
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 export default function App() {
   return (
     <SafeAreaProvider>
